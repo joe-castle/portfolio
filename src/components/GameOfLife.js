a47aae8945b38fd8e9501f3d124d4c1803e4452c
Joe Smith <joesmith0488@gmail.com>
2016-09-20 14:54:38 +0100

    gameoflife working with canvas

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -12,94 +12,94 @@ const checkNeighbours = (position, cells) => {
 
   // Checks nodes in the top left corner
 	if (position === 0) {
-		if (cells[l-1].props.alive) neighbours +=1;
-		if (cells[l-w].props.alive) neighbours +=1;
-		if (cells[l-w+1].props.alive) neighbours +=1;
-		if (cells[w-1].props.alive) neighbours +=1;
-		if (cells[1].props.alive) neighbours +=1;
-		if (cells[w*2-1].props.alive) neighbours +=1;
-		if (cells[w].props.alive) neighbours +=1;
-		if (cells[w+1].props.alive) neighbours +=1;
+		if (cells[l-1].alive) neighbours +=1;
+		if (cells[l-w].alive) neighbours +=1;
+		if (cells[l-w+1].alive) neighbours +=1;
+		if (cells[w-1].alive) neighbours +=1;
+		if (cells[1].alive) neighbours +=1;
+		if (cells[w*2-1].alive) neighbours +=1;
+		if (cells[w].alive) neighbours +=1;
+		if (cells[w+1].alive) neighbours +=1;
   // Checks nodes in the top right corner
 	} else if (position === w-1) {
-		if (cells[l-2].props.alive) neighbours +=1;
-		if (cells[l-1].props.alive) neighbours +=1;
-		if (cells[l-w].props.alive) neighbours +=1;
-		if (cells[w-2].props.alive) neighbours +=1;
-		if (cells[0].props.alive) neighbours +=1;
-		if (cells[w*2-2].props.alive) neighbours +=1;
-		if (cells[w*2-1].props.alive) neighbours +=1;
-		if (cells[w].props.alive) neighbours +=1;
+		if (cells[l-2].alive) neighbours +=1;
+		if (cells[l-1].alive) neighbours +=1;
+		if (cells[l-w].alive) neighbours +=1;
+		if (cells[w-2].alive) neighbours +=1;
+		if (cells[0].alive) neighbours +=1;
+		if (cells[w*2-2].alive) neighbours +=1;
+		if (cells[w*2-1].alive) neighbours +=1;
+		if (cells[w].alive) neighbours +=1;
   // Checkes nodes in the bottom left corner
 	} else if (position === l-w) {
-		if (cells[l-w-1].props.alive) neighbours +=1;
-		if (cells[position-w].props.alive) neighbours +=1;
-		if (cells[position-w+1].props.alive) neighbours +=1;
-		if (cells[l-1].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
-		if (cells[w-1].props.alive) neighbours +=1;
-		if (cells[0].props.alive) neighbours +=1;
-		if (cells[1].props.alive) neighbours +=1;
+		if (cells[l-w-1].alive) neighbours +=1;
+		if (cells[position-w].alive) neighbours +=1;
+		if (cells[position-w+1].alive) neighbours +=1;
+		if (cells[l-1].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
+		if (cells[w-1].alive) neighbours +=1;
+		if (cells[0].alive) neighbours +=1;
+		if (cells[1].alive) neighbours +=1;
   // Checks nodes in the bottom right corner
 	} else if (position === l-1) {
-		if (cells[l-w-2].props.alive) neighbours +=1;
-		if (cells[l-w-1].props.alive) neighbours +=1;
-		if (cells[l-w*2].props.alive) neighbours +=1;
-		if (cells[l-2].props.alive) neighbours +=1;
-		if (cells[l-w].props.alive) neighbours +=1;
-		if (cells[w-2].props.alive) neighbours +=1;
-		if (cells[w-1].props.alive) neighbours +=1;
-		if (cells[0].props.alive) neighbours +=1;
+		if (cells[l-w-2].alive) neighbours +=1;
+		if (cells[l-w-1].alive) neighbours +=1;
+		if (cells[l-w*2].alive) neighbours +=1;
+		if (cells[l-2].alive) neighbours +=1;
+		if (cells[l-w].alive) neighbours +=1;
+		if (cells[w-2].alive) neighbours +=1;
+		if (cells[w-1].alive) neighbours +=1;
+		if (cells[0].alive) neighbours +=1;
   // Checks nodes in top row (excluding the corners)
 	} else if (position >= 1 && position <= w-2) {
-		if (cells[l-w+position].props.alive) neighbours +=1;
-		if (cells[l-w-1+position].props.alive) neighbours +=1;
-		if (cells[l-w+1+position].props.alive) neighbours +=1;
-		if (cells[position-1].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
-		if (cells[position+w+1].props.alive) neighbours +=1;
-		if (cells[position+w].props.alive) neighbours +=1;
-		if (cells[position+w-1].props.alive) neighbours +=1;
+		if (cells[l-w+position].alive) neighbours +=1;
+		if (cells[l-w-1+position].alive) neighbours +=1;
+		if (cells[l-w+1+position].alive) neighbours +=1;
+		if (cells[position-1].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
+		if (cells[position+w+1].alive) neighbours +=1;
+		if (cells[position+w].alive) neighbours +=1;
+		if (cells[position+w-1].alive) neighbours +=1;
   // Checks nodes in bottom row (excluding corners)
 	} else if (position >= l-w+1 && position <= l-2) {
-		if (cells[position-w-1].props.alive) neighbours +=1;
-		if (cells[position-w].props.alive) neighbours +=1;
-		if (cells[position-w+1].props.alive) neighbours +=1;
-		if (cells[position-1].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
-		if (cells[w-(l-position)-1].props.alive) neighbours +=1;
-		if (cells[w-(l-position)].props.alive) neighbours +=1;
-		if (cells[w-(l-position)+1].props.alive) neighbours +=1;
+		if (cells[position-w-1].alive) neighbours +=1;
+		if (cells[position-w].alive) neighbours +=1;
+		if (cells[position-w+1].alive) neighbours +=1;
+		if (cells[position-1].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
+		if (cells[w-(l-position)-1].alive) neighbours +=1;
+		if (cells[w-(l-position)].alive) neighbours +=1;
+		if (cells[w-(l-position)+1].alive) neighbours +=1;
   // Checks nodes in the left column (excluding corners)
 	} else if (position % w === 0) {
-		if (cells[position-1].props.alive) neighbours +=1;
-		if (cells[position-w].props.alive) neighbours +=1;
-		if (cells[position-w+1].props.alive) neighbours +=1;
-		if (cells[position+w-1].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
-		if (cells[position+w*2-1].props.alive) neighbours +=1;
-		if (cells[position+w].props.alive) neighbours +=1;
-		if (cells[position+w+1].props.alive) neighbours +=1;
+		if (cells[position-1].alive) neighbours +=1;
+		if (cells[position-w].alive) neighbours +=1;
+		if (cells[position-w+1].alive) neighbours +=1;
+		if (cells[position+w-1].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
+		if (cells[position+w*2-1].alive) neighbours +=1;
+		if (cells[position+w].alive) neighbours +=1;
+		if (cells[position+w+1].alive) neighbours +=1;
   // Checks nodes in the right column (excluding corners)
 	} else if (position % w === w-1) {
-		if (cells[position-w-1].props.alive) neighbours +=1;
-		if (cells[position-w].props.alive) neighbours +=1;
-		if (cells[position-w*2+1].props.alive) neighbours +=1;
-		if (cells[position-1].props.alive) neighbours +=1;
-		if (cells[position-w+1].props.alive) neighbours +=1;
-		if (cells[position+w-1].props.alive) neighbours +=1;
-		if (cells[position+w].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
+		if (cells[position-w-1].alive) neighbours +=1;
+		if (cells[position-w].alive) neighbours +=1;
+		if (cells[position-w*2+1].alive) neighbours +=1;
+		if (cells[position-1].alive) neighbours +=1;
+		if (cells[position-w+1].alive) neighbours +=1;
+		if (cells[position+w-1].alive) neighbours +=1;
+		if (cells[position+w].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
   // Checks all other nodes in the middle
 	} else {
-		if (cells[position-w-1].props.alive) neighbours +=1;
-		if (cells[position-w].props.alive) neighbours +=1;
-		if (cells[position-w+1].props.alive) neighbours +=1;
-		if (cells[position-1].props.alive) neighbours +=1;
-		if (cells[position+1].props.alive) neighbours +=1;
-		if (cells[position+w-1].props.alive) neighbours +=1;
-		if (cells[position+w].props.alive) neighbours +=1;
-		if (cells[position+w+1].props.alive) neighbours +=1;
+		if (cells[position-w-1].alive) neighbours +=1;
+		if (cells[position-w].alive) neighbours +=1;
+		if (cells[position-w+1].alive) neighbours +=1;
+		if (cells[position-1].alive) neighbours +=1;
+		if (cells[position+1].alive) neighbours +=1;
+		if (cells[position+w-1].alive) neighbours +=1;
+		if (cells[position+w].alive) neighbours +=1;
+		if (cells[position+w+1].alive) neighbours +=1;
 	}
 	return neighbours;
 }
@@ -110,25 +110,6 @@ const gliderGun = (i) => [500, 501, 570, 571, 510, 580, 650, 721, 441, 372, 373,
 const switchEngine = (i) => [1414, 1415, 1416, 1417, 1418, 1419, 1420, 1421, 1423, 1424, 1425, 1426, 1427, 1431, 1432, 1433, 1440, 1441, 1442, 1443, 1444, 1445, 1446, 1448, 1449, 1450, 1451, 1452].some(x => x === i);
 
 const pufferTrain = (i) => [906, 907, 908, 975, 976, 977, 978, 979, 1044, 1045, 1047, 1048, 1049, 1115, 1116, 1322, 1324, 1385, 1391, 1394, 1454, 1455, 1456, 1457, 1458, 1462, 1464, 1523, 1524, 1528, 1529, 1531, 1532, 1594, 1602, 1665, 1666, 1669, 1672, 1743, 1805, 1806, 1809, 1812, 1874, 1882, 1943, 1944, 1948, 1949, 1951, 1952, 2014, 2015, 2016, 2017, 2018, 2022, 2024, 2085, 2091, 2094, 2162, 2164, 2375, 2376, 2444, 2445, 2447, 2448, 2449, 2515, 2516, 2517, 2518, 2519, 2586, 2587, 2588].some(x => x === i);
-
-// Components
-const Cell = ({
-	id,
-	old,
-	alive,
-	handleClick
-}) => (
-	<div
-		id={id}
-		onClick={() => handleClick(id)}
-		className={classNames({
-			'GameOfLife__grid__cell': true,
-			'GameOfLife__grid__cell--dead': !alive,
-			'GameOfLife__grid__cell--alive': alive,
-			'GameOfLife__grid__cell--old': old
-		})}
-	/>
-);
 
 class GameOfLife extends React.Component {
 	constructor() {
@@ -167,10 +148,37 @@ class GameOfLife extends React.Component {
 				return this.clear();
 			}
 
-			this.setState(({
+			this.setState({
 				cells: this.changeCells(),
 				generation: this.state.generation += 1
-			}));
+			});
+
+      // this.grid.fillStyle = '#37474f';
+      // this.grid.fillRect(0, 0, 70 * 12, 50 * 12);
+
+      if (this.grid) {
+        let column = 0;
+        let row = 0;
+        
+        this.state.cells.forEach(cell => {
+          // Draw the cell
+          this.grid.fillStyle = this.getFillStyle(cell);
+          this.grid.fillRect(column, row, 12, 12);
+
+          // Give the cell a border
+          this.grid.fillStyle = '#263238';
+          this.grid.strokeRect(column, row, 12, 12);
+
+          // Move to the next cell
+          column += 12;
+
+          // Move to the next row
+          if (column >= 840) {
+            column = 0;
+            row += 12;
+          }
+        });
+      }
 
 			this.clearTimer();
 			this.generationTimer = setTimeout(startFn, this.state.speed);
@@ -181,6 +189,20 @@ class GameOfLife extends React.Component {
 			this.generationTimer = setTimeout(startFn, this.state.speed)
 		}
 	}
+
+  getFillStyle = cell => {
+    if (cell.dead) {
+      return '#37474f';
+    }
+
+    if (cell.old) {
+      return '#1565c0';
+    }
+
+    if (cell.alive) {
+      return '#29b6f6';
+    }
+  }
 
 	clear = () => {
 		this.clearTimer();
@@ -201,7 +223,7 @@ class GameOfLife extends React.Component {
 		this.setState(state => ({
 			cells: [
 				...state.cells.slice(0, id),
-				this.createCellNode(id, !state.cells[id].props.alive),
+				this.createCellNode(id, !state.cells[id].alive),
 				...state.cells.slice(id + 1)
 			]
 		}));
@@ -232,12 +254,13 @@ class GameOfLife extends React.Component {
 	// }
 
 	changeCells = () => {
-		let nextCells = [], cells = this.state.cells;
+    const cells = this.state.cells;
+    let nextCells = [];
 
 		for (let id = 0, totalCells = 3500; id < totalCells; id++) {
 		  let aliveNeighbours = checkNeighbours(id, cells);
 
-		  if (cells[id].props.alive) {
+		  if (cells[id].alive) {
 			 if (aliveNeighbours >= 4 || aliveNeighbours <= 1) {
 				nextCells.push(this.createCellNode(id, false));
 			 } else {
@@ -253,42 +276,43 @@ class GameOfLife extends React.Component {
 		return nextCells;
 	}
 
-	createCellNode = (id, alive, old = false) => (
-		<Cell
-			handleClick={this.handleCellClick}
-			id={id}
-			key={id}
-			alive={alive}
-			old={old}
-		/>
-	)
+	createCellNode = (id, alive, old = false) => ({
+    id,
+    alive,
+    old,
+	})
 
 	checkAmountAlive = () => (
-		this.state.cells.reduce((x, y) => {
-			if (y.props.alive) {
-        return x + 1
-      }
-
-			return x;
-		}, 0)
+		this.state.cells.reduce((amountAlive, cell) => (cell.alive ? amountAlive + 1 : amountAlive), 0)
 	)
 
 	render() {
     return (
       <div className="GameOfLife">
         <h1 className="GameOfLife__generation">Generations: {this.state.generation}</h1>
-        <canvas ref="grid" className="GameOfLife__grid">
+        <canvas ref="grid" className="GameOfLife__grid" width={70 * 12} height={50 * 12}>
           <div className="GameOfLife__grid">
-            {this.grid ? null : this.state.cells}
+            {this.grid ? null : this.state.cells.map(cell => (
+              <div
+            		id={cell.id}
+            		onClick={() => this.handleCellClick(cell.id)}
+            		className={classNames({
+            			'GameOfLife__grid__cell': true,
+            			'GameOfLife__grid__cell--dead': !cell.alive,
+            			'GameOfLife__grid__cell--alive': cell.alive,
+            			'GameOfLife__grid__cell--old': cell.old
+            		})}
+            	/>
+            ))}
           </div>
         </canvas>
         <div className="GameOfLife__options">
           <input onClick={this.start} type="button" value="Run"/>
           <input onClick={this.pause} type="button" value="Pause"/>
           <input onClick={this.clear} type="button" value="Clear"/>
-          <input onClick={() => this.setState({speed: 160})} type="button" value="Slow"/>
-          <input onClick={() => this.setState({speed: 80})} type="button" value="Normal"/>
-          <input onClick={() => this.setState({speed: 10})} type="button" value="Fast"/>
+          <input onClick={() => this.setState({ speed: 160 })} type="button" value="Slow"/>
+          <input onClick={() => this.setState({ speed: 80 })} type="button" value="Normal"/>
+          <input onClick={() => this.setState({ speed: 10 })} type="button" value="Fast"/>
           {/*for setting up patterns
           <input onClick={this.aliveCellsId} type="button" value="Alive Cells"/>*/}
         </div>
